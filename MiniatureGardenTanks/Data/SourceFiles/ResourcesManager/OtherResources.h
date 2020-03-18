45e50be04d70a7d726ff2102615c66a4b5c2f1f4
takumyon <taku5145563@gmial.com>
2020-03-18 16:43:02 +0900

    リリース用にゲームをビルドしなおし

--- MiniatureGardenTanks/Data/SourceFiles/ResourcesManager/OtherResources.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include <string>
-
-/*-------------------------------------------*/
-/* その他のリソースを管理する
-/*-------------------------------------------*/
-const std::string STAGEWALL_MAPDATA_TITLE  = "Data/ResourceFiles/Others/Stage/WallMapData_Title.csv";		// タイトルでのステージの配置データ
-const std::string STAGEWALL_MAPDATA_MAIN   = "Data/ResourceFiles/Others/Stage/WallMapData_Main.csv";	// メインでのステージの配置データ
-const std::string STAGEWALL_MAPDATA_RESULT = "Data/ResourceFiles/Others/Stage/WallMapData_Result.csv";	// リザルトでのステージの配置データ
---
+++ MiniatureGardenTanks/gamedata/Data/SourceFiles/Others/AsyncLoadData.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include <Windows.h>
+
+// イメージファイルのパラメータ
+struct ImageFileParam
+{
+	void* fileImage;	// ファイルのイメージデータ
+	int   fileSize;		// ファイルサイズ
+};
+
+// イメージファイルを読み込む
+int LoadImageFile(const TCHAR *filePath, void **fileImageAddr, int *fileSize);
+
+// イメージファイル読み込み用コールバック関数
+int LoadImageFileFunc(const TCHAR *filePath, void **fileImageAddr, int *fileSize, void *fileReadFuncData);
+
+// イメージファイル解放用コールバック関数
+int ReleaseFileFunc(void *memoryAddr, void *fileReadFuncData);
